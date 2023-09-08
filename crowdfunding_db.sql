c7dfeab93ed91f72a324ffa41e772848f99c1558
pathfindereleven <njmizerny@rocketmail.com>
2023-09-07 19:18:58 -0700

    sql

--- crowdfunding_db.sql
+++ crowdfunding_db.sql
@@ -20,23 +20,23 @@
 CREATE TABLE subcategory
 (
 	subcategory_id VARCHAR(8) NOT NULL,
-	subcategory_name VARCHAR(30),
+	subcategory_name VARCHAR(30) ,
 	PRIMARY KEY(subcategory_id)
 );
  -- Create campaign table
  CREATE TABLE campaign(
 	cf_id INT NOT NULL,
-	contact_id INT,
+	contact_id INT NOT NULL,
 	company_name VARCHAR(100),
 	description VARCHAR(100),
-	goal NUMERIC,
-	pledge NUMERIC,
+	goal FLOAT NOT NULL,
+	pledged FLOAT NOT NULL,
 	outcome VARCHAR(100),
-	backers_count INT,
+	backers_count INT NOT NULL,
 	country VARCHAR(2) NOT NULL,
 	currency VARCHAR(3),
-	launched_date DATE,
-	end_date DATE,
+	launched_date DATE NOT NULL,
+	end_date DATE NOT NULL,
 	category_id VARCHAR(4) NOT NULL,
 	subcategory_id VARCHAR(8) NOT NULL,		
 	PRIMARY KEY(cf_id)
@@ -45,6 +45,10 @@ CREATE TABLE subcategory
 -- Add constraint to tables after create/import above data
 ALTER TABLE campaign
 ADD CONSTRAINT fk_category_campaign FOREIGN KEY(category_id) REFERENCES category (category_id),
-ADD CONSTRAINT fk_contacts_campaign FOREIGN KEY(contact_id) REFERENCES contacts (contact_id);
-ADD CONSTRAINT fk_subcategory_campaign FOREIGN KEY(subcategory_id) REFERENCES subcategory (subcategory_id),
+ADD CONSTRAINT fk_contacts_campaign FOREIGN KEY(contact_id) REFERENCES contacts (contact_id),
+ADD CONSTRAINT fk_subcategory_campaign FOREIGN KEY(subcategory_id) REFERENCES subcategory (subcategory_id);
 
+SELECT * FROM category;
+SELECT * FROM contacts;
+SELECT * FROM subcategory;
+SELECT * FROM campaign;
